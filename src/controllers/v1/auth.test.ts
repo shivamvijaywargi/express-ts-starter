720f9aade6af7ba1bf500c9407b4d3b276182c55
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-04-25 12:28:10 +0530

    test: add test for login route✅

--- src/controllers/v1/auth.test.ts
+++ src/controllers/v1/auth.test.ts
@@ -45,3 +45,52 @@ describe('Test the registration route POST /api/v1/auth/new', () => {
     expect(response.body.name).toBe('ZodError');
   });
 });
+
+describe('Test the login route POST /api/v1/auth', () => {
+  it('should return email not verified error', async () => {
+    const response = await request(app).post('/api/v1/auth').send({
+      email: 'jest@unit.test',
+      password: '12345678',
+    });
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.message).toBe('Account not verified, please verify.');
+  });
+
+  it('should return user not found error', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth')
+      .send({
+        email: 'jest1@unit.test',
+        password: '12345678',
+      })
+      .expect(400);
+
+    expect(response.body.message).toBe(
+      'Invalid email or password or user does not exist.',
+    );
+  });
+
+  it('should return invalid email address', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth')
+      .send({
+        email: 'jest1@unit.test11',
+        password: '12345678',
+      })
+      .expect(400);
+
+    expect(response.body.name).toBe('ZodError');
+  });
+
+  it('should return a 400 status code when a required field is missing', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth')
+      .send({
+        email: 'jest@unit.test',
+      })
+      .expect(400);
+
+    expect(response.body.name).toBe('ZodError');
+  });
+});
