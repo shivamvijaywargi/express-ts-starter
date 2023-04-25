77c2bb3395a4c7e38e041f753add5c185383e56b
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-04-25 14:25:41 +0530

    feat: add verify token, resend verification token✨

--- src/controllers/v1/auth.controller.ts
+++ src/controllers/v1/auth.controller.ts
@@ -24,6 +24,8 @@ const cookieOptions: ICookieOptions = {
   // secure: true, // This does not work in local I believe
 };
 
+const CLIENT_URL = process.env.CLIENT_URL;
+
 /**
  * @REGISTER
  * @ROUTE @POST {{URL}}/api/v1/auth/new
@@ -58,7 +60,7 @@ export const registerUser: RequestHandler = asyncHandler(
       token: verificationToken,
     });
 
-    const emailLink = `${process.env.CLIENT_URL}/api/v1/auth/verify/${emailVerificationToken.token}/${newUser._id}`;
+    const emailLink = `${CLIENT_URL}/api/v1/auth/verify/${emailVerificationToken.token}/${newUser._id}`;
 
     const payload = {
       name: newUser.firstName,
@@ -189,3 +191,115 @@ export const logoutUser: RequestHandler = asyncHandler(async (req, res) => {
 
   res.status(200).json(sendResp('Logged out successfully.', {}, 200));
 });
+
+/**
+ * @VERIFY_USER_EMAIL
+ * @ROUTE @GET {{URL}}/api/v1/auth/verify/:token/:userId
+ * @returns Email verified successfully
+ * @ACCESS Public
+ */
+export const verifyUserEmail: RequestHandler = asyncHandler(
+  async (req, res, next) => {
+    const { token, userId } = req.params;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return next(new AppErr('User not found.', 404));
+    }
+
+    if (user.isEmailVerified) {
+      return next(
+        new AppErr('Your email has already been verified. Please login.', 404),
+      );
+    }
+
+    const emailVerificationToken = await VerifyResetToken.findOne({
+      _userId: user._id,
+      token,
+    });
+
+    if (!emailVerificationToken) {
+      return next(new AppErr('Invalid token or token expired.', 400));
+    }
+
+    user.isEmailVerified = true;
+
+    await user.save();
+
+    if (user.isEmailVerified) {
+      const emailLink = `${CLIENT_URL}/login`;
+
+      const payload = {
+        name: user.firstName,
+        link: emailLink,
+      };
+
+      await sendEmail(
+        user.email,
+        'Welcome - Account Verified',
+        payload,
+        '../template/welcome.handlebars',
+      );
+
+      return res.redirect('/auth/verify');
+    }
+  },
+);
+
+/**
+ * @RESEND_VERIFY_EMAIL_TOKEN
+ * @ROUTE @POST {{URL}}/api/v1/auth/verify/resend
+ * @returns Email verification token sent successfully
+ * @ACCESS Public
+ */
+export const resendVerifyEmailToken: RequestHandler = asyncHandler(
+  async (req, res, next) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next(new AppErr('User not found.', 404));
+    }
+
+    if (user.isEmailVerified) {
+      return next(
+        new AppErr('Your email has already been verified. Please login.', 404),
+      );
+    }
+
+    // Find if token exists
+    const verificationTokenExists = await VerifyResetToken.findOne({
+      _userId: user._id,
+    });
+
+    // Delete if token exists
+    if (verificationTokenExists) {
+      await verificationTokenExists.deleteOne();
+    }
+
+    const verificationToken = randomBytes(32).toString('hex');
+
+    const emailVerificationToken = await VerifyResetToken.create({
+      _userId: user._id,
+      token: verificationToken,
+    });
+
+    const emailLink = `${CLIENT_URL}/api/v1/auth/verify/${emailVerificationToken.token}/${user._id}`;
+
+    const payload = {
+      name: user.firstName,
+      link: emailLink,
+    };
+
+    await sendEmail(
+      user.email,
+      'Account Verification',
+      payload,
+      '../templates/accountVerification.handlebars',
+    );
+
+    res.status(200).json(sendResp(`Email verification token sent.`, {}, 200));
+  },
+);
